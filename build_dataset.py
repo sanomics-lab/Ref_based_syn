8dcd896c165f848b5d8116aecc3e95b31418d537
yaodaojj2018 <35592413+yaodaojj2018@users.noreply.github.com>
2024-01-08 13:46:59 +0800

    Update build_dataset.py

--- build_dataset.py
+++ build_dataset.py
@@ -1,8 +1,10 @@
 import os
 import numpy as np
 from rdkit import Chem
+from rdkit.Chem import AllChem, DataStructs
 from decomposer import BreakMol
 from tqdm import tqdm
+from utils_new import process_reac_file
 
 
 def strip_dummy_atoms(mol):
@@ -32,18 +34,17 @@ def search_fragements(smiles, building_blocks, bb_emb, nBit=256):
     rank_idx = sim_arr.argsort()[::-1]
     similar_frags = []
     threshold = 0.5
-    for i in range(top_k):
+    for i in range(len(rank_idx)):
         if sim_arr[rank_idx[i]] >= threshold:
             similar_frags.append(building_blocks[rank_idx[i]])
-
     return list(set(similar_frags))
 
 ref_smi = ['CC1=CC=C(NC(=O)CCCN2CCN(C/C=C/C3=CC=CC=C3)CC2)C(C)=C1']
 query_frags = []
 for rs in ref_smi:  
     mol = Chem.MolFromSmiles(rs)
-    for i in range(1, 3):
-        bm = BreakMol(mol, lower_limit=5, cut_num=2)
+    for c in range(1, 3):
+        bm = BreakMol(mol, lower_limit=5, cut_num=c)
         for frags, break_bonds, ori_index in bm.enumerate_break():
             frags = [Chem.MolToSmiles(strip_dummy_atoms(i)) for i in frags]
             query_frags.extend(frags)
@@ -56,13 +57,11 @@ with open('data/query_frags.txt', 'w') as f:
 all_bbs = []
 with open('data/zinc_frags_in_stock.txt', 'r') as f:
     building_blocks = [l.strip().split()[0] for l in f.readlines()]
-bb_fps = np.load('data/zinc_emb_fp_1024.npy')
+bb_fps = np.load('data/zinc_emb_fp_256.npy')
 for cf in tqdm(query_frags):
     similar_bbs = search_fragements(cf, building_blocks, bb_fps, nBit=256)
     all_bbs.extend(similar_bbs)
 all_bbs = list(set(all_bbs))
 print('all_bbs :', len(all_bbs))
-
-with open('data/similar_bbs.txt', 'w') as f:
-    for bbs in all_bbs:
-        f.write(bbs + '\n')
+        
+process_reac_file('data/rxn_set_uspto.txt', all_bbs)        
